151c22d98e665ddb66a57cd8d3307e983216787a
neicore <neicoreadams@gmail.com>
2022-01-04 14:26:37 +0300

    Working on v2.0

---
+++ app.js
@@ -0,0 +1,17 @@
+let deleteCourseButtons = Array.from(
+  document.querySelectorAll('.delete-course')
+)
+
+let deleteCourse = (node) => {
+  node.parentNode.parentNode.remove()
+
+  //   DCB -> Delete Course Buttons
+  let remainingDCB = Array.from(document.querySelectorAll('.delete-course'))
+  if (remainingDCB.length === 1) remainingDCB[0].style.display = 'none'
+}
+
+deleteCourseButtons.forEach((deleteCourseButton) => {
+  deleteCourseButton.addEventListener('click', () => {
+    deleteCourse(deleteCourseButton)
+  })
+})
